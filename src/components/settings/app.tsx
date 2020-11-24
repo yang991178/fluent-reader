7a286de5aa44f63a635875953ea97588da5247c3
刘浩远 <yang991178@live.com>
2020-11-24 11:48:28 +0800

    update locales

--- src/components/settings/app.tsx
+++ src/components/settings/app.tsx
@@ -107,8 +107,8 @@ class AppTab extends React.Component<AppTabProps, AppTabState> {
         { key: "en-US", text: "English" },
         { key: "es", text: "Español" },
         { key: "fr-FR", text: "Français" },
+        { key: "tr", text: "Türkçe" },
         { key: "zh-CN", text: "中文（简体）" },
-        { key: "tr", text: "Türkçe" },    
     ]
 
     toggleStatus = () => {
