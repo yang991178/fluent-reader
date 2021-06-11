e02d1987c6ad7825457416bc8fe505324b43ab6e
TianchengHu <3139658+tiancheng2000@users.noreply.github.com>
2021-06-11 12:53:09 +0800

    feat-i18n-ja (#259)

--- src/scripts/i18n/_locales.ts
+++ src/scripts/i18n/_locales.ts
@@ -1,6 +1,7 @@
 import en_US from "./en-US.json"
 import zh_CN from "./zh-CN.json"
 import zh_TW from "./zh-TW.json"
+import ja from "./ja.json"
 import fr_FR from "./fr-FR.json"
 import de from "./de.json"
 import nl from "./nl.json"
@@ -12,6 +13,7 @@ const locales = {
     "en-US": en_US,
     "zh-CN": zh_CN,
     "zh-TW": zh_TW,
+    "ja": ja,
     "fr-FR": fr_FR,
     "de": de,
     "nl": nl,
