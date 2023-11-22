0deb012570060b9a2e049f77260536153b0a926d
AoEiuV020 <aoeiuv020@gmail.com>
2023-11-22 13:07:41 +0800

    删除cookies日志

--- src/main/cookies.ts
+++ src/main/cookies.ts
@@ -5,7 +5,6 @@ import { BrowserWindow, WebContents } from "electron"
 const cookieStore = new Store()
 const COOKIES_KEY = "cookiesKey"
 export async function saveCookies(webContents: WebContents) {
-    console.log('save cookie')
     const currentSession = webContents.session
     await currentSession.cookies.flushStore()
     let cookies = await currentSession.cookies.get({})
@@ -13,13 +12,11 @@ export async function saveCookies(webContents: WebContents) {
         ...cookie,
         url:(cookie.secure ? 'https://' : 'http://') + cookie.domain.replace(/^\./, '') + cookie.path
     }))
-    console.log('save cookie: ' + JSON.stringify(customCookies))
     cookieStore.set(COOKIES_KEY, customCookies)
 }
 export async function loadCookies(webContents: WebContents) {
     const savedCookies = cookieStore.get(COOKIES_KEY)
     cookieStore.get
-    console.log('load cookie: ' + JSON.stringify(savedCookies))
     if (savedCookies && savedCookies.length > 0) {
         const currentSession = webContents.session
         savedCookies.forEach(cookie => {
